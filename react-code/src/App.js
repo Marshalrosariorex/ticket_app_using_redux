2d058e37c4f04396851ef9d4936311e55d6ea256
Marshalrosariorex <rosariorex20@gmail.com>
2024-08-09 12:03:41 +0530

    src file changes

---
+++ react-code/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddTicket from './components/AddTicket';
+
+function App() {
+  return (
+    <div className="App">
+      <AddTicket />
+    </div>
+  );
+}
+
+export default App;
